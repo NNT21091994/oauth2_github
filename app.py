e4ba3cb808fb7be434bd8bf145d4134f1701a0e7
tanwita_nimiar <tanwita.nimiar@knowlarity.com>
2016-03-02 17:02:45 +0530

    hello

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def demo():
 
 @app.route("/myapp/callback", methods=['GET'])
 def callback():
-	github = OAuth2Session(client_id, state=session["oauth_state"])
+	github = OAuth2Session(client_id, state=session['oauth_state'])
 	token = github.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
 	session["oauth_token"] = token
 	return redirect(url_for('.profile'))
