114154c304acf268ad8d7462fc8b7d542220178c
tanwita_nimiar <tanwita.nimiar@knowlarity.com>
2016-03-02 17:10:26 +0530

    hello

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ token_url = "https://github.com/login/oauth/access_token"
 
 @app.route("/myapp")
 def myapp():
+	session["hello"]="hello"
 	return render_template('view.js')
 
 
@@ -24,9 +25,10 @@ def myapp():
 def demo():
 	try:
 		github = OAuth2Session(client_id)
-		authorization_url, state = github.authorization_url(authorization_base_url)
+		return render_template('err.html',err=session["hello"]
+		"""authorization_url, state = github.authorization_url(authorization_base_url)
 		session['oauth_state'] =  state
-		return redirect(authorization_url)
+		return redirect(authorization_url)"""
 	except Exception as e:
 		traceback.print_exc(file=sys.stdout)
 		return render_template('err.html',err=str(e))
