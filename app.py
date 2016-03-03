12ac3af8b9a62e6be28f1376649e30f5a78e372c
tanwita_nimiar <tanwita.nimiar@knowlarity.com>
2016-03-03 11:19:32 +0530

    hello

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def myapp():
 def demo():
    try:
     github = OAuth2Session(client_id)
-    authorization_url, state = github.authorization_url(authorization_base_url)
+    authorization_url,code, state = github.authorization_url(authorization_base_url)
     session['oauth_state'] = state
     return render_template('err.html',err=authorization_url)
     #return redirect(authorization_url)
