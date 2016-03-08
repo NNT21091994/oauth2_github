0f3dec3f01262608b95588fccd81dc8bcab561c3
tanwita_nimiar <tanwita.nimiar@knowlarity.com>
2016-03-08 15:36:07 +0530

    DDSF

--- app.py
+++ app.py
@@ -38,8 +38,9 @@ def demo():
 @app.route("/myapp/callback", methods=['GET'])
 def callback():
 	github = OAuth2Session(client_id, state=session.get('oauth_state'))
-	token = github.fetch_token(token_url, username=client_id, password=client_secret, authorization_response=request.url)
-	return render_template('err.html',err=authorization_response)
+	return render_template('err.html',err=request.url)
+	#token = github.fetch_token(token_url, username=client_id, password=client_secret, authorization_response=request.url)
+	
 	#session['oauth_token'] = token
 	#return jsonify(github.get('https://api.github.com/user').json())
 	#return redirect(url_for('.profile'))
